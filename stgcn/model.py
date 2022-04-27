ee0ff0a02a5fe086d47c2ffe6ed4b649b89ef45d
Mikhael Belmiro <belmiro533@gmail.com>
2022-04-27 14:49:40 +0700

    added further improvements

--- stgcn/model.py
+++ stgcn/model.py
@@ -1,19 +1,19 @@
 from torch import nn
-from torch_geometric_temporal import STConv
+from torch_geometric_temporal import STConv, TemporalConv
 
 class STGCN(nn.Module):
-    def __init__(self, num_nodes, in_channels, hidden_channels, out_channels, kernel_size, K):
+    def __init__(self, in_timesteps, out_timesteps, num_nodes, in_channels, hidden_channels, out_channels, kernel_size, K):
         super().__init__()
+        self.in_timesteps = in_timesteps
+        self.out_timesteps = out_timesteps
         self.first_stconv = STConv(num_nodes, 1, hidden_channels, out_channels, kernel_size, K)
         self.second_stconv = STConv(num_nodes, out_channels, hidden_channels, out_channels, kernel_size, K)
-        self.final_conv = None
-        self.linear =  nn.Linear(2,2)
+        self.final_conv = TemporalConv(out_channels, out_channels, self.in_timesteps-4*(kernel_size-1))
+        self.linear =  nn.Linear(out_channels, 1)
 
     def forward(self, x, edge_index, edge_weight):
         out_1 = self.first_stconv(x, edge_index, edge_weight)
-        print(out_1.shape)
         out_2 = self.second_stconv(out_1, edge_index, edge_weight)
-        print(out_2.shape)
-        out_3 = self.final_conv(out_2)
+        out_3 = self.final_conv(out_2).squeeze()
         out = self.linear(out_3)
         return out
